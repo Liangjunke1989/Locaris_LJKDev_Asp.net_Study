0e669ef57509277f74969a5ea901e44fab610f4a
LJKGit <liangjunke198909@outlook.com>
2020-06-04 10:34:43 +0800

    20200604_LJKDev_分页操作03

--- Locaris_LJKDev_Asp.NetStudy02/Locaris.LJKDev_Asp.NetStudy.Common/PageBarHelper.cs
+++ Locaris_LJKDev_Asp.NetStudy02/Locaris.LJKDev_Asp.NetStudy.Common/PageBarHelper.cs
@@ -9,45 +9,57 @@ namespace Locaris.LJKDev_Asp.NetStudy.Common
     {
         public static string GetPageBar(int pageIndex, int pageCount)
         {
-            if (pageCount==1)
+            //00_如果数据页数只有一页的情况下，不需要使用PageBar
+            if (pageCount == 1)
             {
                 return string.Empty;
             }
-            int start = pageIndex - 3;//计算起始位置，要求页面上显示6个数字页码
-
-            if (start<1)
+            //01_计算起始位置,pageIndex在中间，作为中位数，往前数两个，作为显示的起始数字
+            int start = pageIndex - 2;
+            if (start < 1)
             {
                 start = 1;
             }
-            int end = start + 6;//计算终止位置
 
-            if (end>pageCount)
+            //02_计算出当前显示的末尾页面，当前显示的终止页面
+            int end = start + 5;
+            if (end > pageCount)
             {
+                //end的最大值不超过pageCount
                 end = pageCount;
-                start = end - 6 < 1 ? 1 : end - 6;
+                start = end - 5 < 1 ? 1 : end - 5;
             }
-            StringBuilder stringBuilder=new StringBuilder();
-            if (pageIndex>1)
+
+            //03_为页面添加超链接
+            StringBuilder stringBuilder = new StringBuilder();
+            //03_01为页面添加"上一页"超链接
+            if (pageIndex > 1)
             {
-                stringBuilder.AppendFormat("<a href='NewList.aspx?pageIndex={0}'>上一页</a>",pageIndex-1);
+                stringBuilder.AppendFormat("<a href='NewList.aspx?pageIndex={0}'>上一页</a>", pageIndex - 1);
             }
-            for (int i = start; i <=end; i++)
+            //03_02为页面添加<a></a>超链接
+            for (int i = start; i <= end; i++)
             {
-                if (i==pageIndex)
+                if (i == pageIndex)
                 {
-                    stringBuilder.Append(i);
+                    //03_02_01如果是当前页，当前页不用添加超链接
+                    stringBuilder.Append(" " + i + " ");
                 }
                 else
                 {
+                    //03_02_02把其他页都添加上超链接<a></a>标签
                     stringBuilder.AppendFormat("<a href='NewList.aspx?pageIndex={0}'> {0} </a>  ", i);
                 }
             }
-
-            if (pageIndex<pageCount)
+            //03_03为页面添加"下一页"超链接
+            if (pageIndex < pageCount)
             {
                 stringBuilder.AppendFormat("<a href='NewList.aspx?pageIndex={0}'>下一页</a>", pageIndex + 1);
             }
+
+            //04_返回字符串
             return stringBuilder.ToString();
         }
     }
 }
+
